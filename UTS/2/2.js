cf7730b58e0f888d842b63ce4db6bf59cf144bf7
Joshua Mihai Daniel Nadeak <joshua.mihai@ui.ac.id>
2023-04-09 22:55:56 +0700

    Push For Fixed

--- UTS/2/2.js
+++ UTS/2/2.js
@@ -1,189 +1,165 @@
 "use strict";
 
+var canvas;
 var gl;
-var vertexCount;
 
-var theta = 0.0;
-var thetaLoc;
+var maxPoints = 5000;
+var maxNumPositions  = 3*maxPoints;
+var index = 0;
+var midpoints = [];
+// Accuracy of point : range from 0 to 1 (smaller is better, but slower)
+var pointAccuracy = 0.009;  
 
-var delay = 100;
-var direction = true;
+// program variables (program, buffer, color, and position)
+var program;
+var vBuffer;
+var positionLoc;
+var cBuffer;
+var colorLoc;
 
-
-var xA, yA, xB, yB;
-var listx = [];
-var listy = [];
-var pointCount;
-var listLines = [];
-
-init();
-
-function init() {
-    var canvas = document.getElementById("gl-canvas");
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
 
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
 
-    //
-    //  Configure WebGL
-    //
+
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1, 0.94, 0.94, 1.0);
+    gl.clearColor(0.3,0.6,1, 1.0);
 
+    
+    //  Load shaders & Initialize attribute buffers
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+    
+    // Even handler (set point to each)
 
+    document.getElementById("octagram").addEventListener("click",function() {
+        initvBuffer();
+        // Draw octagram from point
+        mply(-1,-1,0,1);    // south west to north
+        mply(1,-1,0,1);     // south east to north
+        mply(0,-1,-1,1);    // south to north west
+        mply(0,-1,1,1);     // south to north east
 
-    document.getElementById("confirm").onclick = function () {
-        // Menerima start dan end point
-        xA = document.getElementById("x1").value;
-        yA = document.getElementById("y1").value;
-        xB = document.getElementById("x2").value;
-        yB = document.getElementById("y2").value;
+        mplx(-1,0,1,1);     // west to nort east
+        mplx(-1,0,1,-1);    // west to south east
+        mplx(-1,-1,1,0);    // south west to east
+        mplx(-1,1,1,0);     // north west to east
 
-        // Melakukan algoritma MidpointLine
-        MidpointLine(xA, xB, yA, yB, listx, listy); 
+        draw();
+    });
+    document.getElementById("fan").addEventListener("click",function() {
+        initvBuffer();
+        // Draw fan from point
+        for (var i=-1;i<=1;i+=0.1) mply(i,-1,-1,1);
+        draw();
+    });
 
-        pointCount = listy.length;
+    document.getElementById("wind").addEventListener("click",function() {
+        initvBuffer();
+        mplx(-1,1,1,-1);    // nort west to south east
+        mplx(-1,-1,1,1);    // south west to north east
+        mplx(-1,0,1,0);     // west to east
+        mply(0,-1,0,1);     // south to north
+        draw();
+    });
+    
+    draw();
 
-        listLines.length = 0;
-
-        // Membuat verteks dari titik 
-        for (let i = 0; i < pointCount; i++) {
-            var current_x = listx[i];
-            var current_y = listy[i];
-            listLines.push(vec2(current_x / 10, current_y / 10));
-        }
-
-        //  Load shaders and initialize attribute buffers
-
-        var program = initShaders(gl, "vertex-shader", "fragment-shader");
-        gl.useProgram(program);
-
-        // Load the data into the GPU
-
-        var bufferId = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(listLines), gl.STATIC_DRAW);
-
-        // Associate out shader variables with our data buffer
-
-        var positionLoc = gl.getAttribLocation(program, "aPosition");
-        gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(positionLoc);
-
-        thetaLoc = gl.getUniformLocation(program, "uTheta");
-
-        render();
-    }
-};
-
-function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    gl.drawArrays(gl.LINE_STRIP, 0, pointCount);
 }
 
-// Algoritma midpoint line
+//Usable from line with degree of 45 to 90.
+//leftmost point first --> then rightmost.
 
-function MidpointLine(xA, xB, yA, yB, listx, listy) {
-    var Po, dx, dxAbs, dy, dyAbs, xk, yk;
-
-    dx = xB - xA;
-    dy = yB - yA;
-
-    dxAbs = Math.abs(dx);
-    dyAbs = Math.abs(dy);
-
-    listx.length = 0;
-    listy.length = 0;
-
-    listx.push(parseInt(xA));
-    listy.push(parseInt(yA));
-
-    if (dy > dx || dy <= 0) {
-        if (dy > dx && dy > 0) {
-            Po = (2 * dxAbs) - dyAbs;
-            xk = xA;
-            yk = yA;
-
-            while (yA < yB) {
-                yA++;
-
-                if (Po < 0) {
-                    yk++;
-                    Po = Po + (2 * dxAbs);
-                } else {
-                    xk++;
-                    yk++;
-                    Po = Po + (2 * dxAbs) - (2 * dyAbs);
-                }
-
-                listx.push(parseInt(xk));
-                listy.push(parseInt(yk));
-            }
-        } else {
-            if (dy >= (dx * -1)) {
-                Po = (2 * dyAbs) - dxAbs;
-                xk = xA;
-                yk = yA;
-
-                while (xA < xB) {
-                    xA++;
-
-                    if (Po < 0) {
-                        xk++;
-                        Po = Po + (2 * dyAbs);
-                    } else {
-                        xk++;
-                        yk--;
-                        Po = Po + (2 * dyAbs) - (2 * dxAbs);
-                    }
-
-                    listx.push(parseInt(xk));
-                    listy.push(parseInt(yk));
-                }
-            } else {
-                if (dy < (dx * -1)) {
-                    Po = (2 * dyAbs) - dxAbs;
-                    xk = xA;
-                    yk = yA;
-
-                    while (yA > yB) {
-                        yA--;
-
-                        if (Po < 0) {
-                            yk--;
-                            Po = Po + (2 * dxAbs);
-                        } else {
-                            xk++;
-                            yk--;
-                            Po = Po + (2 * dxAbs) - (2 * dyAbs);
-                        }
-
-                        listx.push(parseInt(xk));
-                        listy.push(parseInt(yk));
-                    }
-                }
-            }
+function mply(x1,y1,x2,y2) { // use polarity to add/subtract y
+    var xi = x1;
+    var yi = y1;
+    if (x1>x2) var polarity = -1;
+    else var polarity = 1;
+    midpoints.push(vec2(xi,yi));
+    
+    var dx = Math.abs(x2 - x1);
+    var dy = Math.abs(y2 - y1);
+    var D = 2 * dx - dy;
+    var incE = 2 * dx;
+    var incNE = 2 * (dx - dy);
+    while (yi<y2) {
+        if (D <= 0) {
+            D = D + incE;
         }
-    } else {
-        Po = (2 * dyAbs) - dxAbs;
-        xk = xA;
-        yk = yA;
-
-        while (xA < xB) {
-            xA++;
-
-            if (Po < 0) {
-                xk++;
-                Po = Po + (2 * dyAbs);
-            } else {
-                xk++;
-                yk++;
-                Po = Po + (2 * dyAbs) - (2 * dxAbs);
-            }
-
-            listx.push(parseInt(xk));
-            listy.push(parseInt(yk));
+        else {
+            D = D + incNE;
+            xi += pointAccuracy*polarity;
         }
+        yi += pointAccuracy;
+        midpoints.push(vec2(xi,yi));
     }
+}
+
+//Usable from line with degree of 0 to 45.
+//leftmost point --> rightmost point.
+
+function mplx(x1,y1,x2,y2) {
+    var xi = x1;
+    var yi = y1;
+    if (y1>y2) var polarity = -1;
+    else var polarity = 1;
+    midpoints.push(vec2(xi,yi));
+    
+    var dx = Math.abs(x2 - x1);
+    var dy = Math.abs(y2 - y1);
+    var D = 2 * dy - dx;
+    var incE = 2 * dy;
+    var incNE = 2 * (dy - dx);
+    while (xi<x2) {
+        if (D <= 0) {
+            D = D + incE;
+        }
+        else {
+            D = D + incNE;
+            yi += pointAccuracy*polarity;
+        }
+        xi += pointAccuracy;
+        midpoints.push(vec2(xi,yi));
+    }
+}
+
+// Initialize Buffer
+function initvBuffer() {
+    index = 0;
+    midpoints = [];
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 8*maxNumPositions, gl.STATIC_DRAW);
+
+    positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumPositions, gl.STATIC_DRAW );
+
+    colorLoc = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colorLoc);
+}
+
+function draw() {
+    // load data to buffer
+    for (let i in midpoints) {
+        gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+        var t = midpoints[i];
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        t = vec4(1,1,1,1);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(t));
+        index++;
+    }
+    // Draw Arrays
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    for (var i = 0; i<index; i++)
+        gl.drawArrays(gl.POINTS, 0, index);
 }
